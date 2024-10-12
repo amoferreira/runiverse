a02f1b96614d07338509c0497c252df53e793c1f
Margarida Ferreira <anamargaridaferreira08@gmail.com>
2024-10-12 12:11:06 +0100

    Implement the one time event observation

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.runtime.android)
     debugImplementation(libs.androidx.compose.ui.tooling)
     api(libs.androidx.compose.material3)
 
