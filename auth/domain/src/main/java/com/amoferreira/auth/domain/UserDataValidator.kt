6a05fd5861deff8d5cec878948e27312d5cbfee8
Margarida Ferreira <anamargaridaferreira08@gmail.com>
2025-03-01 14:31:47 +0000

    Implement user data validator

--- auth/domain/src/main/java/com/amoferreira/auth/domain/UserDataValidator.kt
+++ auth/domain/src/main/java/com/amoferreira/auth/domain/UserDataValidator.kt
@@ -1,6 +1,26 @@
 package com.amoferreira.auth.domain
 
-class UserDataValidator {
+class UserDataValidator(
+    private val patternValidator: PatternValidator,
+) {
+
+    fun isEmailValid(email: String): Boolean {
+        return patternValidator.matches(email.trim())
+    }
+
+    fun validatePassword(password: String): PasswordValidationState {
+        val hasMinLength = password.length > MIN_PASSWORD_LENGTH
+        val hasNumber = password.any { it.isDigit() }
+        val hasLowerCaseCharacter = password.any { it.isLowerCase() }
+        val hasUpperCaseCharacter = password.any { it.isUpperCase() }
+
+        return PasswordValidationState(
+            hasMinimalLength = hasMinLength,
+            hasNumber = hasNumber,
+            hasLowerCaseCharacter = hasLowerCaseCharacter,
+            hasUpperCaseCharacter = hasUpperCaseCharacter,
+        )
+    }
 
     companion object {
         const val MIN_PASSWORD_LENGTH = 9
